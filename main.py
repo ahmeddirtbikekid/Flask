022ec469bc44d7335804e45abf707b3c5c16daff
ahmeddirtbikekid <95101413+ahmeddirtbikekid@users.noreply.github.com>
2022-04-27 21:02:33 +0300

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from market import app
 
-#Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
     app.run(debug=True)
 #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
